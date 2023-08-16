feddb6111368c83efe3e79efa5b3f8588406ac16
Junenancy <irokajuliet012gmail.com>
2023-08-16 22:22:04 +0100

    dashboard

---
+++ script.js
@@ -0,0 +1,117 @@
+// sidebar toggle
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+
+function openSidebar () {
+    if(!sidebarOpen) {
+        sidebar.classList.add("sidebar-responsive");
+        sidebarOpen = true;
+    }
+}
+
+function closeSidebar() {
+    if(sidebarOpen) {
+        sidebar.classList.remove("sidebar-responsive");
+        sidebarOpen = false;
+    }
+}
+
+// ---------Charts----------
+// ----------BarChart--------------
+
+var barChartOptions = {
+    series: [{
+    data: [10, 8, 6, 4, 2]
+  }],
+    chart: {
+    type: 'bar',
+    height: 350,
+    toolbar: {
+        show: false
+    },
+  },
+  colors: [
+    "#246dec",
+    "#cc3c43",
+    "#367952",
+    "#f535a1",
+    "#4f35a1"
+],
+  plotOptions: {
+    bar: {
+        distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: "40%",
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  legend: {
+    show: false
+  },
+  xaxis: {
+    categories: ["Laptop", "Phones", "Headphones", "Monitor", "Camera"],
+  },
+  yaxis: {
+    title: {
+        text: "count"
+    }
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector(".bar-chart"), barChartOptions);
+  chart.render();
+
+
+  // Area Chart
+
+  var areaChartOptions = {
+    series: [{
+    name: 'Purchase Orders',
+    data: [31, 40, 28, 51, 42, 109, 100]
+  }, {
+    name: 'Sales Orders',
+    data: [11, 32, 45, 32, 34, 52, 41]
+  }],
+    chart: {
+    height: 350,
+    type: 'area',
+    toolbar: {
+    show: false,
+  },
+},
+colors: ["#4f35a1", "#246dec"],
+dataLabels: {
+    enabled: false,
+},
+  stroke: {
+    curve: 'smooth'
+  },
+  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+  markers: {
+    size: 0
+  },
+  yaxis: [
+    {
+      title: {
+        text: 'Purchase Orders',
+      },
+    },
+    {
+      opposite: true,
+      title: {
+        text: 'Sales Orders',
+      },
+    },
+  ],
+  tooltip: {
+    shared: true,
+    intersect: false,
+  }
+  };
+
+  var areaChart = new ApexCharts(document.querySelector(".area-chart"), areaChartOptions);
+  areaChart.render();
